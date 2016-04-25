dbb4934f3f5bca0c149720d0de6d27c29f72750c
Geeku <ggongpei@qq.com>
2016-04-25 21:46:39 +0800

    add title

--- S3/Task 45/js/main.js
+++ S3/Task 45/js/main.js
@@ -24,6 +24,33 @@
             self.imgList = Array.prototype.slice.call($$('img', self.wrap));
             self.padding = param.padding ? param.padding.replace('px', '') : 0;
 
+            //弹出层初始化
+            self.imgPop = $('#xiuPhoto-pop') || null;
+            if(!self.imgPop) {
+                document.body.appendChild(create({id: "xiuPhoto-pop"}));
+                //self.wrap.innerHTML += '<div id="wowPhoto-pop"></div>'; 会导致self.wrap指向不正确
+                self.imgPop = $('#xiuPhoto-pop');
+            }
+
+            //弹出层点击黑色区域隐藏
+            self.imgPop.addEventListener('click', function(event) {
+                event = event || window.event;
+                if(event.target.id === 'xiuPhoto-pop') {
+                    self.imgPop.className = self.imgPop.className.replace('show', '');
+                }
+            });
+
+            $(param.wrap).addEventListener('click', function(event) {
+
+                event = event || window.event;
+                var target = event.target;
+
+                //弹出大图
+                if (target.tagName.toLowerCase() === 'img') {
+                    self.pop({src: target.src});
+                }
+            });
+
             self.createCol(self.imgList);
 
         },
@@ -123,28 +150,75 @@
                     if(!imgList[j]) {
                         return;
                     }
-                    var oHeight = imgList[j].height,
+
+                    //获取title和content
+                    var contentArea = self._getContent(imgList[j]),
+                        oHeight = imgList[j].height,
                         oWidth = imgList[j].width,
                         imgRatio = height / oHeight,
                         item = create({
                             tag: 'li',
-                            className: 'img-item',
+                            className: 'img-item ',
                             id: 'xiuPhoto-item-' + self.imgCounter++,
                             style: {
                                 'width': oWidth * imgRatio + 'px',
                                 'padding': self.padding / 2 + 'px'
                             }
+                        }),
+                        header = create({
+                            tag: 'header',
+                            className: 'item-header ' + contentArea.className,
                         });
+
+
+                    var headerHTML = "<h3>{0}</h3><p>{1}</p>";
+                    header.innerHTML = headerHTML.replacer([contentArea.title, contentArea.content]);
 
                     if((imgList.length - k) < 3) {
                         item.style.width = (self.minHeight / oHeight) * oWidth + 'px';
                     }
 
                     item.appendChild(imgList[j]);
+                    item.appendChild(header);
                     imgRow.appendChild(item);
                 }
             }
 
+        },
+
+        _getContent: function(item) {
+
+            var title,
+                content,
+                className;
+
+            if(item.title) {
+                title = item.title
+            } else if(item.dataset && item.dataset.title) {
+                title = item.dataset.title;
+            } else {
+                title = '';
+            }
+
+            if(item.content) {
+                content = item.content
+            } else if(item.dataset && item.dataset.content) {
+                content = item.dataset.content;
+            } else {
+                content = '';
+            }
+
+            if(!title && !content) {
+                className = 'no-title-content';
+            } else {
+                className = (!title ? 'no-title' : '') + (!content ? 'no-content' : '');
+            }
+
+            return {
+                title: title,
+                content: content,
+                className: className
+            }
         },
 
         load: function(url, dealWith) {
@@ -170,6 +244,22 @@
                     }
                 }
             };
+
+            return this;
+        },
+
+        pop: function (param) {
+
+            var src,
+                self = this,
+                html = '<img src="{0}">';
+
+            //判断传递过来的是图片src还是索引值
+            src = param.src ? param.src : self.imgList[param].src;
+
+            self.imgPop.innerHTML = html.replacer([src]);
+
+            self.imgPop.className += ' show';
 
             return this;
         }
